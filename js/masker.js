11368384260629a69fcaf85d305485d6c042d389
cpc2 <48025245+cpc2@users.noreply.github.com>
2019-03-11 16:37:14 +0100

    Update masker.js

--- js/masker.js
+++ js/masker.js
@@ -472,13 +472,16 @@ function addMask(file) {
   }
 }
 
-var gridMasks = document.getElementById("masks");
+
 function clearMasks(){
   localStorage.removeItem("masks");
 }
 
+var gridMasks = document.getElementById("masks");
+if (localStorage.getItem('masks') != null || localStorage.getItem('masks') != "") {
 var savedMasks = localStorage.getItem("masks");
 var masksArray = savedMasks.split(";");
 for (i=0; i<masksArray.length;i++){
   gridMasks.insertAdjacentHTML('beforeend', "<img width='145' height='145' src=\"" + masksArray[i] + "\" onclick='loadMask(this)' />")
+}
 }
